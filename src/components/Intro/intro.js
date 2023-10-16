be9aca89ec816cc47090b6e7579e4576ad34c032
unknown <yezawhtutmwcdev@gmail.com>
2023-10-17 00:11:54 +0630

    first commit

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,21 +1,33 @@
 import React from "react";
-import './intro.css';
-import bg from '../../assets/image.png';
-import btnImg from '../../assets/hireme.png';
+import "./intro.css";
+import bg from "../../assets/yezaw.png";
+import btnImg from "../../assets/hireme.png";
 import { Link } from "react-scroll";
 
 const Intro = () => {
-    return(
-        <section id="intro">
-            <div className="introContent">
-                <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Ei Ei Lwin</span><br/>Senior Software Developer</span>
-                <p className="introPara">I am a skilled software developer <br/> with experiece of developing a lot of projects</p>
-                <Link><button className="btn"><img src={btnImg} className="btnImg" alt="Hire Me" />Hire Me</button></Link>
-            </div>
-            <img src={bg} alt="Profile" className="bg"/>
-        </section>
-    )
-}
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm <span className="introName">Ye Zaw Htut</span>
+          <br />
+          Software Developer
+        </span>
+        <p className="introPara">
+          I am a skilled software developer <br /> with experiece of developing
+          a lot of projects
+        </p>
+        <Link>
+          <button className="btn">
+            <img src={btnImg} className="btnImg" alt="Hire Me" />
+            Hire Me
+          </button>
+        </Link>
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
+};
 
-export default Intro
+export default Intro;
--- src/components/Skill/skill.js
+++ src/components/Skill/skill.js
@@ -1,39 +1,44 @@
 import React from "react";
-import UIDesign from '../../assets/ui-design.png';
-import WebDesign from '../../assets/website-design.png';
-import AppDesign from '../../assets/app-design.png';
+import UIDesign from "../../assets/ui-design.png";
+import WebDesign from "../../assets/website-design.png";
+import WebDevelopment from "../../assets/web-development.png";
 
-import './skill.css';
+import "./skill.css";
 
 const Skill = () => {
-    return (
-        <section id="skills">
-            <span className="skillTitle">What I do</span>
-            <span className="skillDesc">I am a skilled and passionate web developer</span>
-            <div className="skillBars">
-                <div className="skillBar">
-                    <img src={UIDesign} alt="UI Design" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>UI/UX Design</h2>
-                        <p>This is a demo text, you can write your own content</p>
-                    </div>
-                </div>
-                <div className="skillBar">
-                    <img src={WebDesign} alt="Web Design" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Website Design</h2>
-                        <p>This demo text can be changed while making the production ready website.</p>
-                    </div>
-                </div>
-                <div className="skillBar">
-                    <img src={AppDesign} alt="App Design" className="skillBarImg" />
-                    <div className="skillBarText">
-                        <h2>Application Design</h2>
-                        <p>You can write text related to mobile app development.</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
-export default Skill
+  return (
+    <section id="skills">
+      <span className="skillTitle">What I do</span>
+      <span className="skillDesc">
+        I am a skilled and passionate web developer
+      </span>
+      <div className="skillBars">
+        <div className="skillBar">
+          <img src={UIDesign} alt="UI Design" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>UI/UX Design</h2>
+            <p>This is a demo text, you can write your own content</p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={WebDesign} alt="Web Design" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Website Design</h2>
+            <p>
+              This demo text can be changed while making the production ready
+              website.
+            </p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={WebDevelopment} alt="App Design" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Web Application</h2>
+            <p>You can write text related to mobile app development.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Skill;
